b941ca8eb8eeb9657f8e5b4b91f18913e95b2785
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-04-29 17:07:24 -0400

    add url and session information

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -22,6 +22,39 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getRatesClicked(_ sender: Any) {
+        
+        // STEPS:
+        // 1) Request & Session
+        // 2) Response & Data
+        // 3) Parsing & JSON Serialization
+        
+       // 1)
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=8276b2059e608cf3a1e5f676268b3984")
+        
+        // create a session -- urlsession gets the data and manages transferring tasks with data in a specified network
+        // adding the .shared makes sure that it keeps the same session instance (singleton) even with multiple requests
+        let session = URLSession.shared
+        
+        // completion handler defines what will happen when we start the task
+        // the structure in the tast variable is called "Closure"
+        let task = session.dataTask(with: url!) { (data, response, error) in
+            // if there is an error, return an alert with a message that is taken from the localized description of the error
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                // button to allow user to close the alert message
+                let okButton = UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton) // add the button as an action to the alert
+                self.present(alert, animated: true, completion: nil) // allow application to present the alert
+            } else {
+                // 2)
+                // if you do have data then go ahead and present the data
+                if data != nil {
+                    
+                    
+                }
+            }
+        }
+        
     }
     
 }
